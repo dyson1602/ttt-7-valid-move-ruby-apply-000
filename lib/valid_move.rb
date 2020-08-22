8c5d8bb67b9d473304402f35c97a85109b687f34
Christopher Clark <cclark000@citymail.cuny.edu>
2020-08-22 20:13:21 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,7 @@
 # code your #valid_move? method here
 
 def valid_move?(board, index)
-  if index.between?(0, 8) && board[index] != ("" || " ") && board[index] != ("X" || "O")
+  if index.between?(0, 8) && position_taken !=
     TRUE
   else 
     FALSE
@@ -17,3 +17,6 @@ def position_taken?(board, index)
     TRUE
   end
 end
+
+
+board[index] != ("" || " ") && board[index] != ("X" || "O")
